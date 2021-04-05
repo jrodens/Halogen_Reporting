e97b3fb8b2582dee57bb3ad5fa0cda9c225029ef
jrodens <31210906+jrodens@users.noreply.github.com>
2021-04-05 12:39:00 -0400

    Add files via upload

--- APIbreakdownOpen.py
+++ APIbreakdownOpen.py
@@ -5,9 +5,7 @@ import getpass
 import datetime
 import matplotlib.pyplot as plt
 from openpyxl.styles import Alignment
-import time
 
-start = time.perf_counter()
 #setting up lists for entries
 Application= []
 Ticket= []
@@ -28,8 +26,8 @@ countopen=0
 countdev=0
 countqa=0
 countaccepted=0
-#represents the 9 applications part of the cutover apps for filtered view
-cutoverapps = ['Broadridge', 'Cougar', 'FID', 'Falcon', 'Nucleus', 'Perform', 'COIL', 'Cash Forecasting Database', 'Equity Quant']
+#represents the 11 applications part of the cutover apps for filtered view
+cutoverapps = ['Cougar', 'FID', 'Falcon', 'Nucleus', 'Perform']
 
 #prompt user for authorization and authorize user
 username = input('Enter your user name: ')
@@ -39,8 +37,6 @@ jira = JIRA(basic_auth = (username, password), options = {'server': 'https://jir
 #collect data from JIRA API
 datafromjson = jira.search_issues('project = NYC-IO-Surge-Testing and "Epic Link" = NYCTT-1026',maxResults=10000)
 
-print(f'Finished in loading datafromjson in {round(time.perf_counter()-start, 2)} seconds')
-rightnow = time.perf_counter()
 #Class is used to make comparissons against non-string variables such as the object presented here
 class S(str):
     def __contains__(self, x):
@@ -73,8 +69,7 @@ for issue in datafromjson:
         Projects_Impacted.append(projects_impacted)
         Service.append(service)
         Criticality.append(criticality)
-print(f'Finished in api call in {round(time.perf_counter()-rightnow, 2)} seconds')
-rightnow=time.perf_counter()
+
 newdata=pd.DataFrame()
 newdata.insert(0,"Application", Application)
 newdata.insert(1,"Ticket", Ticket)
@@ -111,9 +106,8 @@ summary = summary.groupby(['Status','Criticality','Application']).size()
 #summaryaccepted = accepted.groupby(['Criticality', 'Application']).size()
 #summaryopen.join(summaryaccepted)
 
-#filename ='O:\Investment API Daily Ticket Metrics -' + str(datetime.datetime.today().strftime('%Y%m%d')) +'.xlsx'
-filename = 'C:\\Users\\' + username + '\\OneDrive - TRowePrice\\Transaction Router Daily Ticket Metrics Priority1-'+ str(datetime.datetime.today().strftime('%Y%m%d')) +'.xlsx'
-print(f'Finished processing data in {round(time.perf_counter()-rightnow, 2)} seconds')
+filename ='O:\Investment API Daily Ticket Metrics -' + str(datetime.datetime.today().strftime('%Y%m%d')) +'.xlsx'
+
 with pd.ExcelWriter(filename) as writer:
     #writes dataframes to Excel workbook & names worksheets
     summary.to_excel(writer,  sheet_name='Summary')
@@ -128,7 +122,6 @@ with pd.ExcelWriter(filename) as writer:
     #Formatting Excel workbook.  Sets border as well as column width
     formater=workbook.add_format({'border':1})
     formater.set_text_wrap()
-    formater.set_bg_color('white')
     worksheet1.set_column('A:A',39.43,formater)
     worksheet2.set_column('A:A',39.43,formater)
     worksheet3.set_column('A:A',39.43,formater)
@@ -169,9 +162,7 @@ with pd.ExcelWriter(filename) as writer:
     worksheet2.autofilter('A1:K1000')
     worksheet3.autofilter('A1:K1000')
     worksheet4.write(0,3, 'Count', formater)
-    worksheet4.set_column('B:B', 10.29, formater)
-    worksheet4.set_column('C:C', 10.43,formater)
-    worksheet4.set_column('D:D', 5.57,formater)
-print(f'Total time of {round(time.perf_counter()-start, 2)} seconds')
-print(f'Your pdf has been saved in {filename}')
+    worksheet4.set_column('B:B', 10.29)
+    worksheet4.set_column('C:C', 10.43)
+    worksheet4.set_column('D:D', 5.57)
 print('Done!')
